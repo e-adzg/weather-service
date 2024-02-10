bf27412a6cdd2f333f96dc63f3d2a523da437bf1
Erikas Adzgauskas <ERIKAS.ADZGAUSKAS.2021@MUMAIL.IE>
2024-02-10 14:42:44 +0000

    Added log config, log request and pod info

--- src/main/java/ie/mu/fyp/weatherservice/controller/WeatherController.java
+++ src/main/java/ie/mu/fyp/weatherservice/controller/WeatherController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class WeatherController {
 
+    private static final Logger logger = LoggerFactory.getLogger(WeatherController.class);
     private final WeatherService weatherService;
 
     public WeatherController(WeatherService weatherService) {
@@ -17,6 +20,7 @@ public class WeatherController {
 
     @GetMapping("/weather")
     public Mono<String> getWeather(@RequestParam String city) {
+        logger.info("Handling request in pod: {}", System.getenv("POD_NAME"));
         return weatherService.getWeatherForCity(city);
     }
 }
